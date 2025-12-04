4606a9579e3d5f1bd8e0e566443524c26f49a24e
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-03 20:24:19 -0800

    Wrap karma command in worker for local development support

--- src/command.ts
+++ src/command.ts
@@ -3,27 +3,35 @@ import { link, unlink } from 'link-unlink';
 import path from 'path';
 import Queue from 'queue-cb';
 import resolveBin from 'resolve-bin-sync';
-import { type CommandCallback, type CommandOptions, installPath } from 'tsds-lib';
+import { type CommandCallback, type CommandOptions, installPath, wrapWorker } from 'tsds-lib';
 import url from 'url';
 
+const major = +process.versions.node.split('.')[0];
+const version = major > 14 ? 'local' : 'stable';
 const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
+const dist = path.join(__dirname, '..');
+const workerWrapper = wrapWorker(path.join(dist, 'cjs', 'command.js'));
 const config = path.join(__dirname, '..', '..', 'assets', 'karma.conf.cjs');
 
-export default function karma(args: string[], options: CommandOptions, callback: CommandCallback) {
+function worker(args: string[], options: CommandOptions, callback: CommandCallback) {
   const cwd: string = (options.cwd as string) || process.cwd();
 
   link(cwd, installPath(options), (err, restore) => {
     if (err) return callback(err);
 
     try {
-      const karma = resolveBin('karma');
+      const karmaBin = resolveBin('karma');
       const tests = args.length ? args[0] : 'test/**/*.test.*';
 
       const queue = new Queue(1);
-      queue.defer(spawn.bind(null, karma, ['start', config, tests], options));
+      queue.defer(spawn.bind(null, karmaBin, ['start', config, tests], options));
       queue.await((err) => unlink(restore, callback.bind(null, err)));
     } catch (err) {
       callback(err);
     }
   });
+}
+
+export default function karma(args: string[], options: CommandOptions, callback: CommandCallback): undefined {
+  version !== 'local' ? workerWrapper('stable', args, options, callback) : worker(args, options, callback);
 }
