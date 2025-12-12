d693ad410a4462f07ce246fe0e45143fdd2cdde3
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-12 11:22:13 -0800

    Move karma configuration to src and update build scripts

--- scripts/build.cjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var fs = require('fs');
-var path = require('path');
-var compiled = path.join(__dirname, '..', 'dist', 'cjs', 'scripts', 'build.js');
-if (fs.existsSync(compiled)) {
-  require(compiled);
-} else {
-  console.log('build: Skipping (dist/ not built yet - run npm run build)');
-}
