38d470ce3060f6d0dfc58a496bbb1a674df29256
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-11 21:24:28 -0800

    Migrate build script to TypeScript and pin dependency versions

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,11 +1,2 @@
 #!/usr/bin/env node
-
-const spawn = require('cross-spawn-cb');
-const path = require('path');
-
-const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
-const bin = isWindows ? process.env.npm_config_prefix : path.join(process.env.npm_config_prefix, 'bin');
-
-spawn(path.join(bin, 'tsds'), ['build'], { cwd: process.cwd(), stdio: 'inherit' }, (err) => {
-  process.exit(err ? 1 : 0);
-});
+require('../dist/cjs/scripts/build.js');
