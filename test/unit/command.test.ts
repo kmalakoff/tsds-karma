ddfb76ba006f084fb9c95e2ba6dc710b66a9564c
Kevin Malakoff <kmalakoff@gmail.com>
2026-08-31 00:48:11 -0700

    Add export tests and update test directory structure

--- test/unit/command.test.ts
+++ test/unit/command.test.ts
@@ -4,8 +4,6 @@ delete process.env.NODE_OPTIONS;
 import spawn from 'cross-spawn-cb';
 import fs from 'fs';
 import { linkModule, unlinkModule } from 'module-link-unlink';
-import os from 'os';
-import osShim from 'os-shim';
 import path from 'path';
 import Queue from 'queue-cb';
 import * as resolve from 'resolve';
@@ -13,12 +11,12 @@ import shortHash from 'short-hash';
 import { installGitRepo } from 'tsds-lib-test';
 import url from 'url';
 
-const tmpdir = os.tmpdir || osShim.tmpdir;
 const resolveSync = (resolve.default ?? resolve).sync;
 
 import karma from 'tsds-karma';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
+const packageRoot = fs.realpathSync(path.join(__dirname, '..', '..'));
 
 // Use a repo that has browser tests
 const GITS = ['https://github.com/kmalakoff/fetch-http-message.git'];
@@ -26,7 +24,7 @@ const GITS = ['https://github.com/kmalakoff/fetch-http-message.git'];
 function addTests(repo: string) {
   const repoName = path.basename(repo, path.extname(repo));
   describe(repoName, () => {
-    const dest = path.join(tmpdir(), 'tsds-karma', shortHash(process.cwd()), repoName);
+    const dest = path.join(packageRoot, '.tmp', 'cache', shortHash(process.cwd()), repoName);
     const modulePath = fs.realpathSync(path.join(__dirname, '..', '..'));
     const modulePackage = JSON.parse(fs.readFileSync(path.join(modulePath, 'package.json'), 'utf8'));
     const nodeModules = path.join(dest, 'node_modules');
