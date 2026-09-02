b5707aafad3eef67c993141f839d316218635774
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:53:02 -0700

    Migrate build script to TypeScript and pin dependencies

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-
-const spawn = require('cross-spawn-cb');
-const path = require('path');
-
-const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
-const bin = isWindows ? process.env.npm_config_prefix : path.join(process.env.npm_config_prefix, 'bin');
-
-spawn(path.join(bin, 'tsds'), ['build'], { cwd: process.cwd(), stdio: 'inherit' }, (err) => {
-  process.exit(err ? 1 : 0);
-});
+var fs = require('fs');
+var path = require('path');
+var compiled = path.join(__dirname, '..', 'dist', 'cjs', 'scripts', 'build.js');
+if (fs.existsSync(compiled)) {
+  require(compiled);
+} else {
+  console.log('build: Skipping (dist/ not built yet - run npm run build)');
+}
